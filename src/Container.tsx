4a9596860e8dfc2a34fb92b1b5654b95a046ff77
Shreyas Kelshikar <shreyaskelshikar19@gmail.com>
2024-01-07 13:58:58 +0530

    removed grid components

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,28 +1,6 @@
 import { useState } from "react";
 import ComponentWrapper from "./utils/ComponentWrapper";
-import styled from "styled-components";
-import { Grid } from "@mui/material";
 import LeftPane from "./LeftPane";
-
-const StyledGrid = styled(Grid)`
-  margin: 0 !important;
-`;
-
-const GridItem = styled(Grid)`
-  padding: 0;
-`;
-
-const LeftGridItem = styled(GridItem)`
-  background-color: red;
-
-  min-height: 100%;
-`;
-
-const RightGridItem = styled(GridItem)`
-  background-color: blue;
-
-  min-height: 100%;
-`;
 
 const Container = () => {
   const [drawerOpen, setDrawerOpen] = useState(true);
@@ -30,12 +8,8 @@ const Container = () => {
 
   return (
     <ComponentWrapper>
-      <StyledGrid container spacing={1}>
-        <LeftGridItem>
-          <LeftPane />
-        </LeftGridItem>
-        <RightGridItem>R</RightGridItem>
-      </StyledGrid>
+      <LeftPane />
+      <div>R</div>
     </ComponentWrapper>
   );
 };
